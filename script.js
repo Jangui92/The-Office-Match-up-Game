db1052dcb4809131b6f2fad3f6ffa0a0ad07a089
Janay Anguiano <janayanguiano@gmail.com>
2021-08-09 17:19:16 -0700

    the grid issue

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('', () => {
+  const cardArray = [
+    {
+      square0: 'puppy',
+      image: 'images/puppytongue.jpg'
+    },
+    { square1: 'puppy', image: 'images/puppytongue.jpg' },
+    { square2: 'puppy2', image: 'images/corgi.jpg' },
+    { square3: 'puppy2', image: 'images/corgi.jpg' }
+  ]
+})
+
+const memoryboard = document.querySelector('.memory-board')
+
+function memoryboard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    let square1 = document.createElement('img')
+    square1.document.
+  
+}
