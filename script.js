71e6053852cc8a5c1e6bbb42bedfa3410a713631
Janay Anguiano <janayanguiano@gmail.com>
2021-08-12 08:25:25 -0700

    surge domain added all images added

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squareArray = [
     {
-      name: 'dwightdummy',
-      image: 'images/dwightdummymask.png'
+      name: 'assistant',
+      image: 'images/assistanttothe.jpeg'
     },
-    { name: 'dwightdummy', image: 'images/dwightdummymask.png' },
+    { name: 'assistant', image: 'images/assistanttothe.jpeg' },
+    { name: 'bearsbeets', image: 'images/bearsbeets.png' },
+    { name: 'bears', image: 'images/bearsbeets.png' },
+    { name: 'boom', image: 'images/boomroasted.jpeg' },
+    { name: 'boom', image: 'images/boomroasted.jpeg' },
+    { name: 'stutter', image: 'images/didistutter.jpeg' },
+    { name: 'stutter', image: 'images/didistutter.jpeg' },
+    { name: 'dinkin', image: 'images/dinkinflicka.png' },
+    { name: 'dinkin', image: 'images/dinkinflicka.png' },
+    { name: 'dasjim', image: 'images/dwightasjim.png' },
+    { name: 'dasjim', image: 'images/dwightasjim.png' },
+    { name: 'dummy', image: 'images/dwightdummymask.png' },
+    { name: 'dummy', image: 'images/dwightdummymask.png' },
+    { name: 'false', image: 'images/dwightfalse.jpeg' },
+    { name: 'false', image: 'images/dwightfalse.jpeg' },
+    { name: 'ignorant', image: 'images/dwightyouignorant.png' },
+    { name: 'ignorant', image: 'images/dwightyouignorant.png' },
+    { name: 'electric', image: 'images/electriccity.jpeg' },
+    { name: 'electric', image: 'images/electriccity.jpeg' },
+    { name: 'beyonce', image: 'images/iambeyonce.jpeg' },
+    { name: 'beyonce', image: 'images/iambeyonce.jpeg' },
+    { name: 'jimasd', image: 'images/jimasdwight.png' },
+    { name: 'jimasd', image: 'images/jimasdwight.png' },
+    { name: 'illkillyou', image: 'images/micaelillkillyou.png' },
+    { name: 'illkillyou', image: 'images/micaelillkillyou.png' },
+    { name: 'no', image: 'images/michael nogodno.jpeg' },
+    { name: 'no', image: 'images/michael nogodno.jpeg' },
     { name: 'prisionmike', image: 'images/prisonmike.jpeg' },
-    { name: 'prisionmike', image: 'images/prisonmike.jpeg' }
-    // {name:,image:}, {name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:},
-    // {name:,image:},{name:,image:}
+    { name: 'prisionmike', image: 'images/prisonmike.jpeg' },
+    { name: 'fire', image: 'images/ryanstarted.jpeg' },
+    { name: 'fire', image: 'images/ryanstarted.jpeg' },
+    { name: 'shesaid', image: 'images/shesaid.jpeg' },
+    { name: 'shesaid', image: 'images/shesaid.jpeg' },
+    { name: 'threatlevel', image: 'images/threatlevel.jpeg' },
+    { name: 'threatlevel', image: 'images/threatlevel.jpeg' },
+    { name: 'whyareyou', image: 'images/whyareyou.png' },
+    { name: 'whyareyou', image: 'images/whyareyou.png' },
+    { name: 'attention', image: 'images/youcouldnt.jpeg' },
+    { name: 'attention', image: 'images/youcouldnt.jpeg' },
+    { name: 'idiot', image: 'images/youreanidiot.jpeg' },
+    { name: 'idiot', image: 'images/youreanidiot.jpeg' }
   ]
   squareArray.sort(() => 0.5 - Math.random())
 
@@ -54,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const flipTwo = squareChosenId[1]
     if (squareChosen[0] === squareChosen[1]) {
       alert(`You found a match!`)
-      squares[flipOne].setAttribute('src', 'iimages/blackback.jpeg')
+      squares[flipOne].setAttribute('src', 'images/blackback.jpeg')
       squares[flipTwo].setAttribute('src', 'images/blackback.jpeg')
       squareWin.push(squareChosen)
     } else {
